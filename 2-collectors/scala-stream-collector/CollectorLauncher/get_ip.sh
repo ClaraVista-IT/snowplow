295c1641d689f4285882380c0b738963c152d0d3
cuiere <ennaceur.safouen@gmail.com>
2015-11-25 17:42:55 +0100

    removing duplicated files

--- 2-collectors/scala-stream-collector/CollectorLauncher/get_ip.sh
+++
@@ -1,5 +0,0 @@
-ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-echo ' LOCAL IP ADRESS '
-echo $ip
-echo 'Editing configuration file'
-sed  -i "s/\(interface *= *\).*/\1\"$ip\"/" collector5.conf
--- 2-collectors/scala-stream-collector/src/main/scala/com.snowplowanalytics.snowplow.collectors/scalastream/ScalaCollectorHttps.scala
+++
@@ -1,8 +0,0 @@
-package com.snowplowanalytics.snowplow.collectors.scalastream
-
-/**
- * Created by safouane on 23/11/15.
- */
-class ScalaCollectorHttps {
-
-}
